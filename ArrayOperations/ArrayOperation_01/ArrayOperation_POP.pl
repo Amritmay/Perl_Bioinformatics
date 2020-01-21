ab2426a9fbe5349a3316e5115440520ee7fd4389
Amritmay <40712949+Amritmay@users.noreply.github.com>
2020-01-21 19:04:24 +0530

    Add files via upload

---
+++ ArrayOperations/ArrayOperation_01/ArrayOperation_POP.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+#To perform various array operations using perl.
+
+@bases = ('A', 'G', 'C', 'T');
+$base1 = pop@bases;
+print "Element removed from the end: $base1\n";
+print "Remaining array of bases: @bases\n";
+EXIT;
+
