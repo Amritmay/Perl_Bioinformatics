b20e7ae6abe0b502f36d36e37ec75a3d11a5d1da
Amritmay <40712949+Amritmay@users.noreply.github.com>
2020-01-21 19:06:08 +0530

    Add files via upload

---
+++ ArrayOperations/ArrayOperations_04/ArrayReverse.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+#Use of reverse command to reverse an array.
+
+@DNA = ('A', 'G', 'C', 'T');
+print "The DNA: @DNA\n";
+@DNAre = reverse@DNA;
+print "The reversed DNA : @DNAre\n";
+
